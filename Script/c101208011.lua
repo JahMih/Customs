84e888413e566bc651b7d1e66bdbbf2ceba2e7f6
JahMih <79441286+JahMih@users.noreply.github.com>
2025-01-19 23:38:20 +0300

    Add cards
    Diabellstar the Sin Adjudicator added

---
+++ Script/c101208011.lua
@@ -0,0 +1,92 @@
+--Diabellstar the Sin Adjudicator
+--Scripted by JahMih
+local s,id=GetID()
+function s.initial_effect(c)
+	--Always treated as a "Sinful Spoils" card
+	local e0=Effect.CreateEffect(c)
+	e0:SetType(EFFECT_TYPE_SINGLE)
+	e0:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_UNCOPYABLE)
+	e0:SetCode(EFFECT_ADD_SETCODE)
+	e0:SetValue(SET_SINFUL_SPOILS)
+	c:RegisterEffect(e0)
+
+	--Special Summon by banishing 1 Spell and 1 Trap from hand/GY
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetType(EFFECT_TYPE_FIELD)
+	e1:SetProperty(EFFECT_FLAG_UNCOPYABLE)
+	e1:SetCode(EFFECT_SPSUMMON_PROC)
+	e1:SetRange(LOCATION_HAND|LOCATION_GRAVE)
+	e1:SetCountLimit(1,id,EFFECT_COUNT_CODE_OATH)
+	e1:SetCondition(s.spproccon)
+	e1:SetTarget(s.spproctg)
+	e1:SetOperation(s.spprocop)
+	c:RegisterEffect(e1)
+
+	--Quick Effect: Pay half LP, target 1 opponent's card, destroy it, then Special Summon if another "Diabell" is on the field
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(id,1))
+	e2:SetCategory(CATEGORY_DESTROY+CATEGORY_SPECIAL_SUMMON)
+	e2:SetType(EFFECT_TYPE_QUICK_O)
+	e2:SetCode(EVENT_FREE_CHAIN)
+	e2:SetRange(LOCATION_MZONE)
+	e2:SetCountLimit(1,{id,1})
+	e2:SetCost(s.cost)
+	e2:SetTarget(s.target)
+	e2:SetOperation(s.operation)
+	c:RegisterEffect(e2)
+end
+s.listed_series={SET_SINFUL_SPOILS,SET_DIABELL}
+
+function s.spfilter(c,tp)
+	return c:IsType(TYPE_SPELL+TYPE_TRAP) and c:IsAbleToRemove(tp,POS_FACEUP,REASON_COST)
+end
+function s.spproccon(e,c)
+	if c==nil then return true end
+	local tp=e:GetHandlerPlayer()
+	return Duel.IsExistingMatchingCard(s.spfilter,tp,LOCATION_HAND|LOCATION_GRAVE,0,1,nil,tp) and 
+		Duel.IsExistingMatchingCard(s.spfilter,tp,LOCATION_HAND|LOCATION_GRAVE,0,1,nil,tp)
+end
+function s.spproctg(e,tp,eg,ep,ev,re,r,rp,chk,c)
+	local g1=Duel.SelectMatchingCard(tp,s.spfilter,tp,LOCATION_HAND|LOCATION_GRAVE,0,1,1,nil,tp)
+	local g2=Duel.SelectMatchingCard(tp,s.spfilter,tp,LOCATION_HAND|LOCATION_GRAVE,0,1,1,nil,tp)
+	g1:Merge(g2)
+	if #g1==2 then
+		g1:KeepAlive()
+		e:SetLabelObject(g1)
+		return true
+	end
+	return false
+end
+function s.spprocop(e,tp,eg,ep,ev,re,r,rp,c)
+	local g=e:GetLabelObject()
+	if g then
+		Duel.Remove(g,POS_FACEUP,REASON_COST)
+		g:DeleteGroup()
+	end
+end
+function s.cost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.CheckLPCost(tp,Duel.GetLP(tp)//2) end
+	Duel.PayLPCost(tp,Duel.GetLP(tp)//2)
+end
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chk==0 then return Duel.IsExistingTarget(nil,tp,0,LOCATION_ONFIELD,1,nil) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DESTROY)
+	local g=Duel.SelectTarget(tp,nil,tp,0,LOCATION_ONFIELD,1,1,nil)
+	Duel.SetOperationInfo(0,CATEGORY_DESTROY,g,1,0,0)
+end
+function s.operation(e,tp,eg,ep,ev,re,r,rp)
+	local tc=Duel.GetFirstTarget()
+	if tc and tc:IsRelateToEffect(e) and Duel.Destroy(tc,REASON_EFFECT)~=0 then
+		if Duel.IsExistingMatchingCard(Card.IsSetCard,tp,LOCATION_MZONE,0,1,nil,SET_DIABELL) then
+			Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+			local g=Duel.SelectMatchingCard(tp,aux.NecroValleyFilter(s.spsynfilter),tp,LOCATION_EXTRA,0,1,1,nil,e,tp)
+			if #g>0 then
+				Duel.SpecialSummon(g,0,tp,tp,false,false,POS_FACEUP)
+			end
+		end
+	end
+end
+function s.spsynfilter(c,e,tp)
+	return (c:IsRace(RACE_ILLUSION) or c:IsRace(RACE_SPELLCASTER)) and c:IsType(TYPE_TUNER+TYPE_SYNCHRO) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+end
